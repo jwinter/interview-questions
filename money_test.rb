15a96554a41c1451d9e85078e5605340039e9824
Stefan Natchev <stefan.natchev@gmail.com>
2014-06-11 10:11:11 -0400

    step 2

--- money_test.rb
+++ money_test.rb
@@ -13,4 +13,9 @@ class MoneyTest < Minitest::Test
     assert_equal 1.05, money.amount
   end
 
+  def test_should_add_money
+    money1 = Money.new("$1.00")
+    money2 = Money.new("$2.00")
+    assert_equal 2.00, money1 + money2
+  end
 end
